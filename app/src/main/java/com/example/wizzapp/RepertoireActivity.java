f60010f5279699c0ec212952c8d40c5f9a27521f
Swaeny <sweeny.972@gmail.com>
2018-07-11 13:23:32 -0400

    - ajout de recycleriew - modification de RepertoireActi et actiityRepertoire.xml - suppression contactadapter - ajout de MyAdapter

--- app/src/main/java/com/example/wizzapp/RepertoireActivity.java
+++ app/src/main/java/com/example/wizzapp/RepertoireActivity.java
@@ -3,6 +3,8 @@ package com.example.wizzapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -10,6 +12,9 @@ import android.widget.Toast;
 
 public class RepertoireActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     public static final String NAME = "com.example.wizzap.Name";
     private String[] ContactNames = {"Tommy", "Adrien", "Veronica", "Andrew", "Meliodas", "Elizabet", "Arlequin"};
 
@@ -19,18 +24,18 @@ public class RepertoireActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repertoire);
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ContactAdapter(this, ContactNames));
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
 
-                Intent intent = new Intent(RepertoireActivity.this, MainActivity.class);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
-                intent.putExtra(NAME, ContactNames[position]);
-                startActivity(intent);
-            }
-        });
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(ContactNames);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
